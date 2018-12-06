755aab9ad223b7ae21227925d424802e7eebd124
Torgny Bjers <torgny.bjers@passportinc.com>
2018-12-06 14:47:02 -0500

    fix broken strings

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,9 +2,9 @@
 #
 # tbjers/flutter ellipsis package
 
-FLUTTER_VERSION=v1.0.0-stable
+FLUTTER_VERSION="v1.0.0-stable"
 FLUTTER_FILE="flutter_macos_${FLUTTER_VERSION}.zip"
-FLUTTER_DIRECTORY=$HOME/flutter
+FLUTTER_DIRECTORY="$HOME/flutter"
 
 pkg.install() {
   brew update
@@ -15,10 +15,10 @@ pkg.install() {
   brew install ios-deploy
   brew install cocoapods
   pod setup
-  [ ! -d "$FLUTTER_DIRECTORY" ] && mkdir -p "$FLUTTER_DIRECTORY"
-  curl -o "/tmp/{$FLUTTER_FILE}" "https://storage.googleapis.com/flutter_infra/releases/stable/macos/${FLUTTER_FILE}"
-  cd "$(dirname "$FLUTTER_DIRECTORY")" || exit "Could not enter base directory to install Flutter."
-  unzip "/tmp/$FLUTTER_FILE"
+  [ ! -d "${FLUTTER_DIRECTORY}" ] && mkdir -p "${FLUTTER_DIRECTORY}"
+  curl -o "/tmp/${FLUTTER_FILE}" "https://storage.googleapis.com/flutter_infra/releases/stable/macos/${FLUTTER_FILE}"
+  cd "$(dirname "${FLUTTER_DIRECTORY}")" || exit "Could not enter base directory to install Flutter."
+  unzip "/tmp/${FLUTTER_FILE}"
   cd "${FLUTTER_DIRECTORY}" || exit "Could not enter Flutter directory."
   [ ! -f "${FLUTTER_DIRECTORY}/bin/flutter" ] && exit "Could not find Flutter binary."
   $("${FLUTTER_DIRECTORY}/bin/flutter") doctor -v
